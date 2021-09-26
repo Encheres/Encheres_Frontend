3a6a2cd224cf4102ca66d4fb432f9c3a83d361c0
tejvirus0003 <tejvirus0003@gmail.com>
2021-09-26 16:53:52 +0530

    contact us page

--- src/Components/contact-us/Contactus.js
+++ src/Components/contact-us/Contactus.js
@@ -2,34 +2,36 @@ import './contactus.css';
 
 const Contactus = () => {
     return (
-        <div className="container">
-            <div className="form-container">
-                <div className="left-container">
-                    <div className="left-inner-container">
-                        <h2>Let's Chat</h2>
-                        <p>Whether you have a question, want to start a project or            simply want to connect.</p>
-                        <br />
-                        <p>Feel free to send me a message in the contact form</p>
+        <div className="body_container" >
+            <div className="contactus_container">
+                <div className="form-container">
+                    <div className="left-container">
+                        <div className="left-inner-container">
+                            <h2>Let's Chat</h2>
+                            <p>Any Feedback from your side will be appreciated</p>
+                            <br />
+                            <p>Feel free to send any feedback</p>
+                        </div>
                     </div>
-                </div>
-                <div className="right-container">
-                    <div className="right-inner-container">
-                        <form action="#">
-                            <h2 className="lg-view">Contact</h2>
-                            <h2 className="sm-view">Let's Chat</h2>
-                            <p>* Required</p>
-                            <div className="social-container">
-                                <a href="google.com" className="social"><i className="fab fa-facebook-f" /></a>
-                                <a href="google.com" className="social"><i className="fab fa-google-plus-g" /></a>
-                                <a href="google.com" className="social"><i className="fab fa-linkedin-in" /></a>
-                            </div>
-                            <input type="text" placeholder="Name *" />
-                            <input type="email" placeholder="Email *" />
-                            <input type="text" placeholder="Company" />
-                            <input type="phone" placeholder="Phone" />
-                            <textarea rows={4} placeholder="Message" defaultValue={""} />
-                            <button>Submit</button>
-                        </form>
+                    <div className="right-container">
+                        <div className="right-inner-container">
+                            <form action="#">
+                                <h2 className="lg-view">Contact</h2>
+                                <h2 className="sm-view">Let's Chat</h2>
+                                <p>* Required</p>
+                                <input type="text" placeholder="Name *" />
+                                <input type="email" placeholder="Email *" />
+                                <p>Type</p>
+                                <input type="radio" id="html" name="fav_language" value="HTML" />
+                                <label for="html">HTML</label>
+                                <input type="radio" id="html" name="fav_language" value="HTML" />
+                                <label for="html">HTML</label>
+                                <input type="radio" id="html" name="fav_language" value="HTML" />
+                                <label for="html">HTML</label>
+                                <textarea rows={4} placeholder="Message" defaultValue={""} />
+                                <button>Submit</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
