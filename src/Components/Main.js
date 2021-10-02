b57fb6e156a95c7034fb8bc8392770ab9bc9658e
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-10-02 13:03:23 +0530

    Merge conflict

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,11 +1,11 @@
 
 import React, { Component } from 'react';
-import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { Route, Redirect, Switch} from 'react-router-dom';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import NewItem from './Create/NewItem';
-import Contact from './Contactus/Contactus'
+import ContactUs from './Contactus/Contactus';
+import SignUp from './Authentication/SignUp';
 class Main extends Component {
 
 
@@ -15,8 +15,10 @@ class Main extends Component {
       <div>
         <Header />
         <Switch>
+          <Route exact path='/signup' component={SignUp} />
+
           {/*Contact us page*/}
-          <Route exact path='/contact-us' component={() => <Contact />} />
+          <Route exact path='/contact-us' component={() => <ContactUs />} />
           <Route exact path='/create/Independent-Item' component={() => <NewItem />} />
           <Redirect to='/create/Independent-Item' />
 
