014f65eba3a03b2c2ce210f02ec5775eb3a4ad19
bhavesh <bkpassi2017@gmail.com>
2021-10-18 13:46:16 +0530

    Added RenderTransaction Component

--- src/Components/Winnings/WinningsListing.js
+++ src/Components/Winnings/WinningsListing.js
@@ -1,14 +1,13 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { Card, CardBody, CardText, CardSubtitle,
     Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
-import { Form, Image } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import { FetchPhysicalAssets } from '../../apis_redux/actions/physicalAsset';
 import AddressForm from '../FrequentComponents/AddressForm';
 import InfiniteScroll from "react-infinite-scroll-component";
-import Identicon from 'react-identicons';
 import Loading from '../loading';
-import Buy_sell from '../../assets/images/sales.jpg';
 import './winnings.css'
 
 class Winnings extends Component{
@@ -19,6 +18,7 @@ class Winnings extends Component{
         this.state={
             page: 0,
             assets: [],
+            detailsFilled: false,
             winnerDetailsModal: false,
             receiverAddress:{
                 addressLine1:"",
@@ -70,146 +70,83 @@ class Winnings extends Component{
         }
     }
 
-    renderWonAsset = (asset) => {
-
-        var assetShowcaseCarousel = [];
-        var showcaseElement;
-        for(var i=0;i<asset.asset.images.length;i++){
-            showcaseElement = {
-                src: "https://ipfs.infura.io/ipfs/"+asset.asset.images[i],
-                altText: "Slide "+i.toString(),
-                key: i.toString()
-            }
-
-            assetShowcaseCarousel.push(showcaseElement);
-        }
-
+    renderTransaction = (asset) => {
         return(
-            <div className='col-12'>
-                <Card id="asset-card-detail">
-                    <CardBody>
-                        <CardSubtitle
-                            tag="h5"
-                            className="mb-4 new-item-card-subtitle"
-                            id="new-item-card-username"
-                        >
-                            {asset.asset.name}
-                        </CardSubtitle>
-                        <div className='row mt-2'>
-                            <div className='col-12 col-lg-4'>
-                                <CardSubtitle tag="h6" className="new-item-preview-price">
-                                    Asset ID{"  "}
-                                    <span style={{ marginLeft: 10, color: "cyan" }}>
-                                        {asset._id}
-                                    </span>
-                                </CardSubtitle>
-                                <CardSubtitle tag="h6" className="new-item-preview-price">
-                                    Pending Payment{"  "}
-                                    <span style={{ marginLeft: 10, color: "cyan" }}>
-                                        {asset.asset.aggregate_base_price+' ETH'}
-                                    </span>
-                                    <span style={{ marginLeft: 10, color: "grey" }}>
-                                        ({(asset.asset.aggregate_base_price*295377.53).toFixed(2)+' Rs'})
-                                    </span>
-                                </CardSubtitle>
-                                <CardSubtitle tag="h6" className="new-item-preview-price">
-                                    Quantity{"  "}
-                                    <span style={{ marginLeft: 10, color: "cyan" }}>
-                                        {asset.asset.quantity}
-                                    </span>
-                                </CardSubtitle>
-                                <CardSubtitle tag="h6" className="new-item-preview-price">
-                                    Pickup Point
-                                    <p style={{ marginTop: 10, color: "cyan" }}>
-                                        { 
-                                            asset.pickup_point.addressLine1+', '+asset.pickup_point.city+', '+
-                                            asset.pickup_point.state+', '+asset.pickup_point.country
-                                        }
-                                    </p>
-                                </CardSubtitle>
-                                <Button id='single-asset-purchase-button' className='mr-4 mt-2'>
+                <Card className='col-11' id='winning-card'>
+                    <div className='row'>
+                        <div className='col-12 col-md-12 col-lg-3'>
+                            <h6 className='winning-card-label rainbow-lr'>Asset ID</h6>
+                            <h6 className='winning-card-detail'>{asset._id}</h6>
+                        </div>
+                        <div className='col-12 col-md-12 col-lg-3'>
+                            <h6 className='winning-card-label rainbow-lr'>Asset Name</h6>
+                            <Link className='winning-card-link' 
+                                to={`/view/independent-physical-assets/${asset._id}`}>
+                                <h6 className='winning-card-detail'>@{asset.asset.name}</h6>
+                            </Link>
+                        </div>
+                        <div className='col-6 col-md-4 col-lg-2'>
+                            <h6 className='winning-card-label rainbow-lr'>From </h6>
+                            <h6 className='mr-4 winning-card-detail'>@{"Ron Johanson"}</h6>
+                        </div>
+                        <div className='col-6 col-md-4 col-lg-2'>
+                            <h6 className='winning-card-label rainbow-lr'>To </h6>
+                            <h6 className='winning-card-detail'>@{"Mario Bridge"}</h6>
+                        </div>
+                        <div className='col-12 col-md-4 col-lg-2'>
+                            {
+                                this.state.detailsFilled ?
+                                <Button id='single-asset-purchase-button'>
                                     Make Payment
-                                </Button>
-                                {" "}
-                                <Button 
+                                </Button> :
+                                <Button
                                     onClick={() => this.setState({
                                         winnerDetailsModal: !this.state.winnerDetailsModal
                                     })}
                                     id='single-asset-purchase-button' className='ml-4 mt-2'>
                                     Receiver Details
                                 </Button>
-                            </div>
-                            <div className='mt-4 col-12 col-lg-6' >
-                                <div style={{display: 'flex', flexDirection: 'row'}}>
-                                    <div style={{marginRight: 20}}>
-                                    <Identicon 
-                                        string={asset.owner ? asset.owner : "0x95002C6453c30b1a3BD6eD8230683BAe5835C3Fd"}
-                                        size={150}
-                                        bg='#ffffff'
-                                    />
-                                    <CardText style={{color: 'cyan'}}>
-                                        From
-                                        <h6 style={{color: 'grey'}}>
-                                            @{"Ron Johanson"}
-                                        </h6>
-                                    </CardText>
-                                    </div>
-                                    <div style={{marginLeft: 20}}>
-                                    <Identicon string={asset.bidder ? asset.bidder : "0x3C0499eC11138D40A36bE1B70324aBF6dbe2Ca31"} 
-                                        size={150}
-                                        bg='#ffffff'
-                                    />
-                                    <CardText className='mr-0' style={{color: 'cyan'}}>
-                                        To
-                                        <h6 style={{color: 'grey'}}>
-                                            @{"Mario Bridge"}
-                                        </h6>
-                                    </CardText>
-                                    </div>
-                                </div>
-                            </div>
+                            }                                
                         </div>
-                    </CardBody>
-                </Card>
-                <Modal isOpen={this.state.winnerDetailsModal}
-                >
-                    <ModalHeader
-                        style={{backgroundColor: '#0B1126', borderWidth: 0}}
-                    >
-                        <div style={{color: 'grey'}}>
-                            Enter Address and Contact
-                        </div>
-                    </ModalHeader>
-                    <ModalBody
-                        style={{backgroundColor: '#0B1126', borderWidth: 0}}
-                    >
-                        <AddressForm address={this.state.receiverAddress} handleAddressChange = {this.handleAddressChange} errors = {this.state.errors.address}/>
-                        <Form.Group className="mb-3" controlId="contact">
-                            <Form.Control
-                                type='number'
-                                name='contact'
-                                className='new-item-form-field' 
-                                placeholder="Contact Number"
-                                style={{backgroundColor: '#03091F', 
-                                    borderWidth: 0,
-                                    color: 'white'
-                                }}
-                                />
-                        </Form.Group>
-                    </ModalBody>
-                    <ModalFooter 
+                    </div>
+                    <Modal isOpen={this.state.winnerDetailsModal} >
+                        <ModalHeader
                             style={{backgroundColor: '#0B1126', borderWidth: 0}}
-                    >
-                        <Button 
-                            className='fa fa-lg fa-telegram' />
-                        <Button 
-                            onClick={() => this.setState({
-                                winnerDetailsModal: !this.state.winnerDetailsModal
-                            })}
-                            className='fa fa-lg fa-times-circle' />
-                    </ModalFooter>
-                </Modal>
-            </div>
+                        >
+                            <div style={{color: 'grey'}}>
+                                Enter Address and Contact
+                            </div>
+                        </ModalHeader>
+                        <ModalBody
+                            style={{backgroundColor: '#0B1126', borderWidth: 0}}
+                        >
+                            <AddressForm address={this.state.receiverAddress} handleAddressChange = {this.handleAddressChange} errors = {this.state.errors.address}/>
+                            <Form.Group className="mb-3" controlId="contact">
+                                <Form.Control
+                                    type='number'
+                                    name='contact'
+                                    className='new-item-form-field' 
+                                    placeholder="Contact Number"
+                                    style={{backgroundColor: '#03091F', 
+                                        borderWidth: 0,
+                                        color: 'white'
+                                    }}
+                                    />
+                            </Form.Group>
+                        </ModalBody>
+                        <ModalFooter 
+                                style={{backgroundColor: '#0B1126', borderWidth: 0}}
+                        >
+                            <Button 
+                                className='fa fa-lg fa-telegram' />
+                            <Button 
+                                onClick={() => this.setState({
+                                    winnerDetailsModal: !this.state.winnerDetailsModal
+                                })}
+                                className='fa fa-lg fa-times-circle' />
+                        </ModalFooter>
+                    </Modal>
+                </Card>
         );
     }
 
@@ -228,13 +165,12 @@ class Winnings extends Component{
         }
         else{
             return(
-                <div className='container'>
+                <div>
                     <div className='row justify-content-center mt-4'>
                         <h3 className='col-12 rainbow-lr new-item-heading'>
                             MY WINNINGS
                         </h3>
                     </div>
-                    <div className='row'>
                         {
                             this.state.assets.length 
                             ?
@@ -250,14 +186,13 @@ class Winnings extends Component{
                                     </h4>
                                 }
                                 >
-                                {this.state.assets.map((asset) => this.renderWonAsset(asset))}
+                                {this.state.assets.map((asset) => this.renderTransaction(asset))}
                             </InfiniteScroll>
                             :
                             <h4 className='col-12 rainbow-lr winnings-end-note'>
                                 That's the end Buddy!!
                             </h4>
                         }
-                    </div>
                 </div>
             );
         }
