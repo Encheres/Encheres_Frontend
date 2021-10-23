30ceb3d4342ed468808988ab45047e0c59742992
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-10-23 13:12:48 +0530

    update live auctions

--- src/apis_redux/reducers/live_auction.js
+++ src/apis_redux/reducers/live_auction.js
@@ -1,5 +1,6 @@
-import {CREATE_LIVE_AUCTION, CREATE_LIVE_AUCTION_FAILED} from '../actions/actionTypes'
+import {CREATE_LIVE_AUCTION, CREATE_LIVE_AUCTION_FAILED, GET_AUCTION_DETAILS, GET_AUCTION_DETAILS_FAILED} from '../actions/actionTypes'
 const INITIAL_STATE = {
+    data: null,
     message:null,
     error:null,
 }
@@ -8,9 +9,13 @@ const INITIAL_STATE = {
 export default (state=INITIAL_STATE, action) => {
     switch(action.type){
         case CREATE_LIVE_AUCTION:
-            return{...state, message:action.payload.message, error:null}
+            return{...state, data:null, message:action.payload.message, error:null}
         case CREATE_LIVE_AUCTION_FAILED:
-            return{...state, error:action.payload.error, message:null}
+            return{...state,data:null, error:action.payload.error, message:null}
+        case GET_AUCTION_DETAILS:
+            return{...state, data:action.payload, error:null, message:null}
+        case GET_AUCTION_DETAILS_FAILED:
+            return{...state, data:null, error:action.payload.error, message:null}
         default:
             return state
     }
