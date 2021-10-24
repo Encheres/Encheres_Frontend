acb0f9d4d2f4bbbcbfab96f8de57e01061821c2a
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-10-24 11:22:37 +0530

    Update live auction

--- src/Components/LiveAuctionRoom/LiveAuctionRoom.js
+++ src/Components/LiveAuctionRoom/LiveAuctionRoom.js
@@ -7,44 +7,21 @@ import {ipfs_base_url} from '../../apis_redux/apis/encheres'
 import {DisplayBadges} from '../FrequentComponents/Category_Badges'
 import ReactPlayer from 'react-player'
 import { Message } from './Message';
-
-import {fetchAuctionDetails, updateAuctionDetails} from '../../apis_redux/actions/live_auction';
-import moment from 'moment';
-
-// const auctionDetails = {
-//     "tags":["Antiques","Households","Collectibles","Mini Items"],
-//     "event_data_time":"",
-//     "pickup_point":{
-//         "addressLine1":"s",
-//         "addressLine2":"sdf",
-//         "city":"asf",
-//         "addressState":"Disneyland",
-//         "postalCode":"111121",
-//         "country":"Disney World"
-//     },
-//     "organizer_contact":"+59848485415",
-//     "items":[{
-//         "name":"Beluga",
-//         "quantity":"1",
-//         "base_price":"0.000025",
-//         "description":"A celebrity cat that is fond of using Discord. You will need to buy a mobile phone and pc for Beluga.",
-//         "images":["QmbsUYfDuTC7mwSm3nua4BenBNjDV9btWM8HrMCJGMm8aZ"],
-//         "bid":{
-//             "price":"0.00025",
-//             "bidder":{
-//                 "anonymous_name":"Jonas"
-
-//             }
-//         },
-//         "video":"QmXSeaxB7P694pgTJX6o8H2fRo2GLX5Ccm584RhjjVreHt"
-
-//     }],
-// }
+import {fetchAuctionDetails, updateUserBids, sellLiveItem} from '../../apis_redux/actions/live_auction';
 
 // 1) Where to store anonymous name
 // 2) Where to store the video
-// 3) Counter
-// 4) Shifting to next bid
+// 3) Shifting to next bid
+// 4) Image not visible in Brave
+// 5) Chat autoscroll correction
+// 6) Multiple api calls for sell. Try backend trigger.
+// 7) Adding check for start time
+
+// reset variables -> bidprice, bid user, my_price, item_index on sell
+const auctionCountdownDuration = 120;//seconds
+const cntOneTimeout = 80;
+const cntTwoTimeout = 40;
+const cntSoldTimeout = 0;
 
 class LiveAuctionRoom extends Component{
     constructor(props){
@@ -61,6 +38,10 @@ class LiveAuctionRoom extends Component{
             cntSoldLeft:true,
             my_price:'',
             base_price:'',
+            time_left:'',
+            minutes_left:'',
+            seconds_left:'',
+            auctionCompleted:false
         }
     }
     componentDidMount = async()=>{
@@ -69,27 +50,42 @@ class LiveAuctionRoom extends Component{
             this.setState({
                 auction_id
             })
-            this.props.fetchAuctionDetails({auction_id});
-           const {REACT_APP_PUSHER_APP_CLUSTER, REACT_APP_PUSHER_KEY} = process.env;
+            await this.props.fetchAuctionDetails({auction_id});
+
+           
+            const {REACT_APP_PUSHER_APP_CLUSTER, REACT_APP_PUSHER_KEY} = process.env;
             this.pusher = new Pusher(REACT_APP_PUSHER_KEY,{
                 cluster:REACT_APP_PUSHER_APP_CLUSTER,
             })
             this.channel = this.pusher.subscribe('auctions');
             this.channel.bind('updated',this.auctionUpdate);
+            if(this.props.liveAuctionDetails.data){
+                const item_index = this.chooseItemIndex();
+                if(item_index){
+                    console.log('item_index',item_index);
+                    this.setItemIndex(item_index);
+                }
+                    
+            }else if(this.props.liveAuctionDetails.error){
+                alert("Something went wrong");
+            }
         }
     }
 
     componentWillUnmount(){
         // unsubscribe from pusher
-        this.pusher.unsubscribe('auctions');
-        console.log('unsubscribed');
+        // this.pusher.unsubscribe('auctions');
+        // console.log('unsubscribed');
     }
 
     auctionUpdate = async()=>{
         const {auction_id} = this.state;
         await this.props.fetchAuctionDetails({auction_id});
+
     }
 
+    /*******************************/
+    // input/onclick Handlers
     nullHandler = (e) =>{
 
     }
@@ -105,9 +101,8 @@ class LiveAuctionRoom extends Component{
         e.preventDefault();
     }
 
-    handleUserBid = e =>{
+    handleUserBid = async (e) =>{
         e.preventDefault();
-        
         let {my_price, bid_price, base_price, auction_id, item_index, anonymous_name} = this.state;
         my_price = parseFloat(my_price);
         bid_price = parseFloat(bid_price);
@@ -121,11 +116,150 @@ class LiveAuctionRoom extends Component{
         const userId = this.props.auth.userId;
         const bid = {bidder:{userId, anonymous_name }, date_time, price:my_price}
         const data= {bid, auction_id, item_id:this.props.liveAuctionDetails.data.items[item_index]._id}
-        // console.log(data);
-        this.props.updateAuctionDetails(data);
+        await this.props.updateUserBids(data);
+
+        if(this.props.bids.message){
+            this.resetTimer();
+        }else{
+            console.log("In wrong");
+
+        }
 
     }
+    sellItem = async(e) =>{
+        let {item_index} = this.state;
+        console.log('Request to sell item: ', item_index)
+        item_index = parseInt(item_index);
+        await this.props.sellLiveItem({auction_id:this.state.auction_id, item_id:this.props.liveAuctionDetails.data.items[item_index]._id});
+        const data = this.props.liveAuctionDetails.data;
+        if(data.completed){
+            this.setState({
+                auctionCompleted:true
+            })
+            return;
+        }
+        if(data.items[item_index].sell.sold){
+            await this.setItemIndex(item_index+1);
+        }
+        
+    }
 
+    /**************************************/
+    // Helper Functions
+
+    chooseItemIndex = () =>{
+        const data = this.props.liveAuctionDetails.data.items;
+        // console.log(data);
+        for(let i=0;i<data.length;i++){
+            if(!data[i].sell.sold){
+                return i;
+            }
+        }
+        this.setState({
+            auctionCompleted: true
+        }, ()=>{
+            console.log('Auction completed from choose fxn');
+        })
+    }
+
+    setItemIndex = async(index) =>{
+        console.log(index);
+        this.setState({
+            time_left:'',
+            minutes_left:'',
+            seconds_left:'',
+            bid_price:'',
+            bid_user:'',
+            my_price:'',
+            cntOneLeft:true,
+            cntTwoLeft:true,
+            cntSoldLeft:true,
+            item_index:index,
+            
+        }, ()=>{
+            console.log("After setting index:", this.state)
+        })
+    }
+
+    /**************************************/
+    // Timer/Counter Functions
+
+    resetTimer = ()=>{
+        this.setState({
+            time_left:'',
+            minutes_left:'',
+            seconds_left:'',
+            cntOneLeft:true,
+            cntTwoLeft:true,
+            cntSoldLeft:true,
+        })
+    }
+    
+    getTimeRemaining = (e) => {
+        const total = Date.parse(e) - Date.parse(new Date());
+        if(total<0){
+            return {
+                total:0,
+                hours:0,
+                minutes:0,
+                seconds:0
+            }
+        }
+        const seconds = Math.floor((total / 1000) % 60);
+        const minutes = Math.floor((total / 1000 / 60) % 60);
+        const hours = Math.floor((total / 1000 * 60 * 60) % 24);
+        return {
+            total: total/1000, hours, minutes, seconds
+        };
+    }
+
+    startTimer = (e)=>{
+        const timer = this.getTimeRemaining(e);
+        let cntOneLeft = true, cntTwoLeft = true, cntSoldLeft = true;
+        if(timer.total<=cntOneTimeout){
+            cntOneLeft = false;
+        }
+        if(timer.total<=cntTwoTimeout){
+            cntTwoLeft = false;
+        }
+        if(timer.total<=cntSoldTimeout){
+            cntSoldLeft = false;
+        }
+
+        this.setState({
+            time_left:timer.total,
+            minutes_left:timer.minutes,
+            seconds_left:timer.seconds,
+            cntOneLeft,
+            cntTwoLeft,
+            cntSoldLeft
+        })
+    }
+
+    renderCounter =  (props)=>{
+        if((this.state.time_left==='' || this.state.time_left>0) && props.bid_date_time){
+            let bidTime = new Date(props.bid_date_time);
+            let expiry_time = new Date( bidTime.getTime()+ auctionCountdownDuration*1000);
+            // let expiry_time = countDownTime.toISOString();
+            if(this.state.time_left===''||this.state.time_left>0){
+                const time_out = setTimeout(async ()=>{
+                    this.startTimer(expiry_time);
+                    if(this.state.time_left<=0){
+                        await this.sellItem();
+                        clearTimeout(time_out);
+                        console.log(time_out);
+                        // return;
+                    }
+                },1000 )
+
+            }else{
+                // clearTimeout();
+            }
+        }
+    }
+
+    /**************************************/
+    // Rendering Components
 
     renderImageCarousel = (images) =>{
         if(!images||images.length<1){
@@ -164,6 +298,10 @@ class LiveAuctionRoom extends Component{
             <p>Video is being displayed here: </p>
         </>)
     }
+
+    
+
+
     
     renderItem = (item)=>{
         if(item.base_price !== this.state.base_price){
@@ -187,7 +325,7 @@ class LiveAuctionRoom extends Component{
                     <div>
                         <span className='item_desc_title'>Highest Bid</span>
                         {this.state.bid_price? <>
-                        <span className='blue_text_inline'><b>{item.bid.price} ETH</b> by <b>{item.bid.bidder.anonymous_name}</b></span>
+                        <span className='blue_text_inline'><b>{item.bid.price} ETH</b> by <b>{item.bid.bidder.anonymous_name} {item.bid.bidder.userId}</b></span>
                         </>:<>
                         <span className='blue_text_inline'> Yet to Begin </span>
                         </>}
@@ -200,12 +338,25 @@ class LiveAuctionRoom extends Component{
                     <Button className='auction_status_buttons' disabled={this.state.cntSoldLeft} onClick={this.handleStatusButtons}>SOLD </Button>
                 </Col>
             </Row>
+
+            {item.bid.price>0 && <Row className="title_row">
+                <div>
+                        <span className='item_desc_title'>Time Left</span>
+                        {this.renderCounter(item.bid)}
+                        <span className='blue_text_inline'><b>{this.state.minutes_left>9?this.state.minutes_left:'0'+this.state.minutes_left} m : 
+                            {this.state.seconds_left>9?this.state.seconds_left:'0'+this.state.seconds_left} s</b> 
+                        </span>
+                </div>
+                </Row>
+            }
+
             <Row className="title_row">
                 <div>
                         <span className='item_desc_title'>Initial Ask Price</span>
                         <span className='blue_text_inline'><b>{item.base_price} ETH</b> </span>
                     </div>
             </Row>
+
 
 
             <div>
@@ -289,76 +440,85 @@ class LiveAuctionRoom extends Component{
 
 
     render(){
+
         if(this.props.liveAuctionDetails.data){
             const auctionDetails = this.props.liveAuctionDetails.data;
-        return(
-            <div className='live_auction_div'>
-                <Row className="heading_section_row">
-                    <h3 className='section_heading rainbow-lr '>
-                        Live Auction
-                    </h3>
-                </Row>
-                <Row className="section_content">
-                    <Col md={8}>
+            if(auctionDetails.completed||this.state.auctionCompleted){
+                return(
+                    <div className='live_auction_div'>
+                    <Row className="heading_section_row">
+                        <h3 className='section_heading rainbow-lr '>
+                            Live Auction
+                        </h3>
+                    </Row>
+
+                    <Row className="section_content">
+                    <Col md={12}>
                         <Card id="singup_form_card" className='auction_card_desc'>
                             <CardBody>
                                 <>
-                                    <div>
-                                        {this.renderItem(auctionDetails.items[this.state.item_index])}
-                                    </div>    
-                                    <div className='auction_details_div'>
-                                        {this.renderAuctionDetails(auctionDetails)}
-
-                                    </div>
-                                    <div className='auction_bidding_input'>
-                                        <h3>Place Your Bid</h3>
-                                        <Row>
-                                            <Col sm={8}>
-                                            <input name='my_price' type='text' className='form-control auction_input_field' value={this.state.my_price} onChange={this.handleInputChange}/>
-                                            </Col>
-                                            <Col sm={4}>
-                                                <Button className='form__button pink_blue_gradiend_btn' onClick={this.handleUserBid}>
-                                                    Place Bid
-                                                </Button>
-                                            </Col>
-                                        </Row>
-                                    </div>
+                                    <span className='item_desc_title'>This auction has already been completed</span>
                                 </>
                             </CardBody>
                         </Card>
                     </Col>
-                    <Col md={4}>
-                        <Card id="singup_form_card" className='chat__section__container'>
-                            <CardBody>
-                                <CardText>
-                                    <h3 className='chat_heading'>Message Center</h3>
-                                    <div className='chat__section_div'>
-                                        {this.renderChats(auctionDetails.chats)}
-                                        {/* {this.renderMessage("$1000, selling item")}
-                                        {this.renderMessage("$1000, selling item")}
-                                        {this.renderMessage("$1000, selling item")}
-                                        {this.renderMessage("$1000, selling item")}
-                                        {this.renderMessage("$1000, selling item")}
-                                        {this.renderMessage("$1000, selling item")}
-                                        {this.renderMessage("$1000, selling item")}
-                                        {this.renderMessage("$1000, selling item")}
-                                        {this.renderMessage("$1000, selling item")}
-                                        {this.renderMessage("$1000, selling item")}
-                                        {this.renderMessage("$1000, selling item")}
-                                        {this.renderMessage("$1000, selling item")}
-                                        {this.renderMessage("$1000, selling item")}
-                                        {this.renderMessage("$1000, selling item")}
-                                        {this.renderMessage("$1000, selling item")}
-                                        {this.renderMessage("$1000, selling item")} */}
-                                    </div>
+                    </Row>
+                   </div>
+                )
+            }else{
+                
+            return(
+                <div className='live_auction_div'>
+                    <Row className="heading_section_row">
+                        <h3 className='section_heading rainbow-lr '>
+                            Live Auction
+                        </h3>
+                    </Row>
+                    <Row className="section_content">
+                        <Col md={8}>
+                            <Card id="singup_form_card" className='auction_card_desc'>
+                                <CardBody>
+                                    <>
+                                        <div>
+                                            {this.renderItem(auctionDetails.items[this.state.item_index])}
+                                        </div>    
+                                        <div className='auction_details_div'>
+                                            {this.renderAuctionDetails(auctionDetails)}
 
-                                </CardText>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Row>
-            </div>
-        )
+                                        </div>
+                                        <div className='auction_bidding_input'>
+                                            <h3>Place Your Bid</h3>
+                                            <Row>
+                                                <Col sm={8}>
+                                                <input name='my_price' type='text' className='form-control auction_input_field' value={this.state.my_price} onChange={this.handleInputChange}/>
+                                                </Col>
+                                                <Col sm={4}>
+                                                    <Button className='form__button pink_blue_gradiend_btn' onClick={this.handleUserBid}>
+                                                        Place Bid
+                                                    </Button>
+                                                </Col>
+                                            </Row>
+                                        </div>
+                                    </>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                        <Col md={4}>
+                            <Card id="singup_form_card" className='chat__section__container'>
+                                <CardBody>
+                                    <CardText>
+                                        <h3 className='chat_heading'>Message Center</h3>
+                                        <div className='chat__section_div'>
+                                            {this.renderChats(auctionDetails.chats)}
+                                        </div>
+
+                                    </CardText>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                </div>
+            )}
         }else{
             return(
                 <div className='live_auction_div'>
@@ -382,8 +542,9 @@ const mapStateToProps = (state, ownProps)=>{
     return({
         ...ownProps,
         auth:state.auth,
-        liveAuctionDetails:state.liveAuction
+        liveAuctionDetails:state.liveAuction,
+        bids:state.bids
     })
 
 }
-export default connect(mapStateToProps, {fetchAuctionDetails, updateAuctionDetails})(LiveAuctionRoom);
+export default connect(mapStateToProps, {fetchAuctionDetails, updateUserBids, sellLiveItem})(LiveAuctionRoom);
