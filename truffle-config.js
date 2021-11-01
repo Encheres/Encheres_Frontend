c2c8339700e7261ec02a2e00a094c0253458253e
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-11-01 11:20:39 +0530

    update gitignore

--- truffle-config.js
+++ truffle-config.js
@@ -1,20 +1,25 @@
+
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+     host: "127.0.0.1",
+     port: 7545,
+     network_id: "*",       // Any network
     },
   },
+
   contracts_directory: './src/contracts/',
-  contracts_build_directory: `./src/abis_1/`,
+  contracts_build_directory: './src/abis/',
+  // Configure your compilers
   compilers: {
     solc: {
-      version: "^0.8.0",
-      optimizer: {
-        enabled: true,
-        runs: 200
+      version: "0.8.9",    // (default: truffle's version)
+      settings: {
+       optimizer: {
+         enabled: true,
+         runs: 200
+       },
       }
     }
-  }
-}
+  },
+};
